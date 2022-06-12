5dfb2816a93860978701cf2f0abe2ce11f109c73
Stricker2 <101269390+Stricker2@users.noreply.github.com>
2022-06-12 01:11:34 -0400

    search button fetches location data from API

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,9 +2,36 @@
 // define variables/elements
 const myKey = '390b272136b9901e3c1b26d843f3138e'
 var searchButton = document.getElementById('search-button')
-var myURL = 'http://api.openweathermap.org/geo/1.0/direct?q=' + $(mySearch) + '&limit=5&appid=' + $(myKey)
+var mySearch = document.getElementById('city-search')
 
 // add functionality to search button
+searchButton.addEventListener('click', function(){
+    console.log('search button has been clicked')
+    var text = $(mySearch).val();
+    console.log(text);
+
+    // var newSearch = $(mySearch).val(text);
+
+    // $(newSearch).trigger('focus')
+
+    // $(newSearch).on('blur', function() {
+    //     var newText = $(newSearch).val();
+    //     console.log(newText);
+    // })
+    var myURL = 'http://api.openweathermap.org/geo/1.0/direct?q=' + text + '&appid=' + myKey
+    
+    fetch(myURL).then(function(response) {
+        if(text == "") {
+            return false
+        } else {
+            console.log(response);
+            response.json().then(function(data) {
+                console.log(data);
+            });
+        }
+    })
+});
+
 // arrange border box for searched city name, date, icon for weather conditions, temperature, humidity, wind speed and UV index
 // add 5 day forecast with date, icon for weather conditions, temp, wind speed and humidity
 // add search history under search 
