f43846f11892214324d75413b7d027a1ce68f9eb
Stricker2 <101269390+Stricker2@users.noreply.github.com>
2022-06-12 17:56:58 -0400

    add uvi and wind speed data

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,6 +9,8 @@ var currentTemp = document.getElementById('current-temp')
 var highTemp = document.getElementById('high-temp')
 var lowTemp = document.getElementById('low-temp')
 var weatherIcon = document.getElementById('weather-icon')
+var uvIndex = document.getElementById('uv-index')
+var windSpeed = document.getElementById('wind-speed')
 
 
 
@@ -26,26 +28,41 @@ searchButton.addEventListener('click', function(){
     //     var newText = $(newSearch).val();
     //     console.log(newText);
     // })
-    var myURL = 'http://api.openweathermap.org/data/2.5/weather?q=' + text + '&appid=' + myKey + '&units=imperial'
+    var myWeatherURL = 'http://api.openweathermap.org/data/2.5/weather?q=' + text + '&appid=' + myKey + '&units=imperial'
     
-    fetch(myURL).then(function(response) {
+    fetch(myWeatherURL).then(function(response) {
         if(text == "") {
             return false
         } else {
-            console.log(response);
             response.json().then(function(data) {
                 console.log(data);
+                // fetch city and temps
                 weatherIcon.src = 'http://openweathermap.org/img/wn/' + data.weather[0].icon + '.png'
                 cityName.innerText = data.name
                 currentTemp.innerText = data.main.temp
                 highTemp.innerText = " " + data.main.temp_max
                 lowTemp.innerText = " " + data.main.temp_min
 
+                // using for collecting uvi and wind speed
+                let cityLat = data.coord.lat
+                let cityLon = data.coord.lon 
+                let dataURL = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + cityLat + '&lon=' + cityLon + '&appid=' + myKey + '&units=imperial'
+
+                // fetch uvi and wind speed
+                fetch(dataURL).then(function(response) {
+                    response.json().then(function(data) {
+                        console.log(data);
+                        uvIndex.innerText = " " + data.current.uvi 
+                        windSpeed.innerText = " " + data.current.wind_speed
+
+                    })
+                })
+                
             });
         }
     })
-});
 
+});
 // arrange border box for searched city name, date, icon for weather conditions, temperature, humidity, wind speed and UV index
 // add 5 day forecast with date, icon for weather conditions, temp, wind speed and humidity
 // add search history under search 
