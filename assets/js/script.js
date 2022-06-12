b6975de6342f8a7ceaf8f8576fc0ca2082034bf9
Stricker2 <101269390+Stricker2@users.noreply.github.com>
2022-06-12 02:53:27 -0400

    add temps and city name

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,14 @@
 const myKey = '390b272136b9901e3c1b26d843f3138e'
 var searchButton = document.getElementById('search-button')
 var mySearch = document.getElementById('city-search')
+var presentWeather = document.getElementById('present-weather')
+var cityName = document.getElementById('city-name')
+var currentTemp = document.getElementById('current-temp')
+var highTemp = document.getElementById('high-temp')
+var lowTemp = document.getElementById('low-temp')
+var weatherIcon = document.getElementById('weather-icon')
+
+
 
 // add functionality to search button
 searchButton.addEventListener('click', function(){
@@ -18,7 +26,7 @@ searchButton.addEventListener('click', function(){
     //     var newText = $(newSearch).val();
     //     console.log(newText);
     // })
-    var myURL = 'http://api.openweathermap.org/geo/1.0/direct?q=' + text + '&appid=' + myKey
+    var myURL = 'http://api.openweathermap.org/data/2.5/weather?q=' + text + '&appid=' + myKey + '&units=imperial'
     
     fetch(myURL).then(function(response) {
         if(text == "") {
@@ -27,6 +35,12 @@ searchButton.addEventListener('click', function(){
             console.log(response);
             response.json().then(function(data) {
                 console.log(data);
+                weatherIcon.src = 'http://openweathermap.org/img/wn/' + data.weather[0].icon + '.png'
+                cityName.innerText = data.name
+                currentTemp.innerText = data.main.temp
+                highTemp.innerText = " " + data.main.temp_max
+                lowTemp.innerText = " " + data.main.temp_min
+
             });
         }
     })
