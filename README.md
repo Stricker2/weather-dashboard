1e935e95c77679b3c159315d7813ab666b050126
Stricker2 <101269390+Stricker2@users.noreply.github.com>
2022-06-12 23:41:23 -0400

    started 5-day forecast and wrote in-progress README

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# weather-dashboard
+# Weather Dashboard (In-progress)
+
+## Description
+
+A web dashboard built with HTML, CSS, and Javascript, where you can search for city by its name and the current day's weather will generate using the OpenWeather API, showing an icon for the current conditions along with the current temperature, the high and low temperature for the day, the UV Index, and the Wind Speed.
+
+## Work In Progress
+
+This is still a work in progress. A later update will show a 5 day forecast of the city searched with each day's date, an icon for the forecasted conditions for each day, the average temps, wind speed, and humidity. Secondly, buttons will be added to store past searches under the search button. These historical buttons will be labeled with the city of the historical search. Lastly, the styling will be updated to provide a more polished viewing experience while using the dashboard.
+
+## Steps Taken
+
+* Basic foundation and styling added to the dashboard using HTML and CSS, along with minor tweaks using Bootstrap CSS.
+
+* Present day weather and data fetched from the OpenWeather API.
+
+## Webpage
+
+Link: https://stricker2.github.io/weather-dashboard/ 
+
+<img src="./assets/images/weather-dashboard-screenshot.png">
+
+## Contributors
+
+Project created by Andrew Secore
--- assets/js/script.js
+++ assets/js/script.js
@@ -11,8 +11,9 @@ var lowTemp = document.getElementById('low-temp')
 var weatherIcon = document.getElementById('weather-icon')
 var uvIndex = document.getElementById('uv-index')
 var windSpeed = document.getElementById('wind-speed')
-
-
+var currentDate = document.getElementById('current-date')
+var nextDate = document.getElementById('next-day')
+var forecastContainer = document.getElementById('forecast-container')
 
 // add functionality to search button
 searchButton.addEventListener('click', function(){
@@ -20,14 +21,6 @@ searchButton.addEventListener('click', function(){
     var text = $(mySearch).val();
     console.log(text);
 
-    // var newSearch = $(mySearch).val(text);
-
-    // $(newSearch).trigger('focus')
-
-    // $(newSearch).on('blur', function() {
-    //     var newText = $(newSearch).val();
-    //     console.log(newText);
-    // })
     var myWeatherURL = 'http://api.openweathermap.org/data/2.5/weather?q=' + text + '&appid=' + myKey + '&units=imperial'
     
     fetch(myWeatherURL).then(function(response) {
@@ -39,14 +32,17 @@ searchButton.addEventListener('click', function(){
                 // fetch city and temps
                 weatherIcon.src = 'http://openweathermap.org/img/wn/' + data.weather[0].icon + '.png'
                 cityName.innerText = data.name
+                // currentDate.innerText =
                 currentTemp.innerText = data.main.temp
                 highTemp.innerText = " " + data.main.temp_max
                 lowTemp.innerText = " " + data.main.temp_min
+                var date = convertTime(data.dt);
+                currentDate.innerText = date
 
                 // using for collecting uvi and wind speed
-                let cityLat = data.coord.lat
-                let cityLon = data.coord.lon 
-                let dataURL = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + cityLat + '&lon=' + cityLon + '&appid=' + myKey + '&units=imperial'
+                var cityLat = data.coord.lat
+                var cityLon = data.coord.lon 
+                var dataURL = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + cityLat + '&lon=' + cityLon + '&exclude=hourly,minutely&appid=' + myKey + '&units=imperial'
 
                 // fetch uvi and wind speed
                 fetch(dataURL).then(function(response) {
@@ -55,14 +51,53 @@ searchButton.addEventListener('click', function(){
                         uvIndex.innerText = " " + data.current.uvi 
                         windSpeed.innerText = " " + data.current.wind_speed
 
+                        
                     })
                 })
-                
+
+                // var forecastCards = []
+                //         for(var i = 0; i < 5; i++) {
+                //             var day = data.daily[i+1]
+                //             console.log(day)
+                //             var forecastCard = generateForecastCard(day)
+                //             forecastCards.push(forecastCard)
+                //         }
+
+                //         for(var j = 0; j < 5; j++) {
+                //             var card = forecastCards[j]
+                //             forecastContainer.append(card)
+                //         }
+
+                //         var generateForecastCard = function() {
+                //             nextDate.innerText = day.dt
+
+                //         }
             });
         }
     })
 
 });
+
+var convertTime = function(timestamp) {
+    let milliseconds = timestamp * 1000
+    var dateGMT = new Date(milliseconds)
+    var localDate = dateGMT.toLocaleString();
+    var date = localDate.split(',')
+    return date[0]
+}
+
+// var generateForecastCard = function() {
+//     var forecastURL = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + cityLat + '&lon=' + cityLon + '&appid=' + myKey + '&units=imperial'
+    
+//     fetch(dataURL).then(function(response) {
+//         response.json().then(function(data) {
+//             console.log(data);
+        
+//             var futureDate = convertTime(day.dt)
+//             nextDay = futureDate
+//         })
+//     })
+// }
 // arrange border box for searched city name, date, icon for weather conditions, temperature, humidity, wind speed and UV index
 // add 5 day forecast with date, icon for weather conditions, temp, wind speed and humidity
 // add search history under search 
